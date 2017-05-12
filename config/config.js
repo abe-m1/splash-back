bb975394f591ebc4e2dd905a56628defe79645dd
abe <abestbet1@gmail.com>
2017-05-12 13:35:40 -0700

    initialize db in config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
+const db = require('./database.js')
 const port = 5000
 
 function init(app){
-    
+    //initialize the database connection
+    app.db = db(dev.db)
 }
 const config = {
     init: init,    
