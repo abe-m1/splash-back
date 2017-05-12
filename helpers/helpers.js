83d71491f2575f0b897340e20b7f9c696b6c8ebb
abe <abestbet1@gmail.com>
2017-05-12 13:46:02 -0700

    create require directory helper function

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,23 @@
 var print = require('./printRoutes')
+var fs = require('fs')
+
+function reqDir(dir) {
+    // Grab the name of the directory to not include it's main file
+    var dirName = dir.split('/')
+    dirName = dirName[dirName.length - 1] + '.js'
+    var reqObj = {};
+    var files = fs.readdirSync(dir);
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        // Don't include the index or directory's main file
+        if (file !== dirName && file !== 'index.js') {
+            reqObj[file] = require(dir + '/' + file);
+        }
+    }
+    return reqObj;
+}
 
 module.exports = {
    printRoutes: print,
+   require: reqDir
 }
