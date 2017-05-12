e7b51cf60b0ad83ff24934bcdf20bb57549c3b1f
abe <abestbet1@gmail.com>
2017-05-12 14:05:29 -0700

    set up website model fields

--- models/Website/websiteModel.js
+++ models/Website/websiteModel.js
@@ -2,4 +2,36 @@ const mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
 
+const Website = {
+    title : String ,
+    favicon: String,
+    category: String,
+    description: String,
+    dateCreated: String,
+    dateLastModified: String,
+    status: String,
+    assignedTo: String,
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    imageUrl: String,
+    headline: String,
+    subheadline: String,
+    step: String,
+    centerIcon : String,
+    rightIcon: String, 
+    leftIcon: String,
+    leftHeadline: String, 
+    centerHeadline: String, 
+    rightHeadline: String, 
+    favoriteColor: String,
+    contactHeadline: String,
+    contactBackground: String,
+    finalized: Boolean,
+    facebookLink: String, 
+    twitterLink: String, 
+    instagramLink: String, 
+    emailAddress: String, 
+    phone: String, 
+    about: String
+}
+
 module.exports = Website
