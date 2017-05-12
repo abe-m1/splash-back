0288293e5823d939068dc7d3632c2156d54054ad
abe <abestbet1@gmail.com>
2017-05-12 14:01:22 -0700

    create userSchema.js

---
+++ models/User/userModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+const userModel = require('./userModel.js')
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+userSchema = new mongoose.Schema(userModel)
+
+userSchema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
+
