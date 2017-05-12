063b549fbcb773f4cc04edeffdad18a57978a5ab
abe <abestbet1@gmail.com>
2017-05-12 14:10:26 -0700

    export required middleware from middleware.js

---
+++ middleware/middleware.js
@@ -0,0 +1,18 @@
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+const cors = require('cors')
+
+
+
+module.exports.init = (app) => {
+ 
+
+
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(cors())
+
+}
