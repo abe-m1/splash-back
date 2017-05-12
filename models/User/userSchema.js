3bf613d5784c79416b8eba7435f1e5b8ef5fcf7b
abe <abestbet1@gmail.com>
2017-05-12 13:59:45 -0700

    complete user schema

--- models/User/userSchema.js
+++ models/User/userSchema.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
-
+const User = {
+    firstName: {type: String},
+    lastName: {type: String},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    website: [{type: Schema.Types.ObjectId, ref: 'Website'}],
+}
 
 module.exports = User
