413d03fc326e05decc04661681dcbbe3c31ad6e4
abe <abestbet1@gmail.com>
2017-05-12 13:53:40 -0700

    function to verify token passed in header

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,3 +9,18 @@ function verifyJwt(token){
 function signJwt(data) {
     return jwt.sign(data, secret)
 }
+
+
+function auth(req, res, next){
+    const token = req.headers['authorization']
+
+    if (!token) throw { message: 'missing token', status: 401 }
+
+    const decoded = verifyJwt(token)
+
+    if (!decoded) throw { message: 'bad token', status: 401 }
+
+    req.decoded = decoded
+
+    next()
+}
