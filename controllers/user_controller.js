6a6c2dfdd21f2ada0754f51dd4ce5f547a39b2ed
abe <abestbet1@gmail.com>
2017-05-19 12:34:58 -0700

    require packages in user controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,11 @@
+const User = require('../models/user')
+const bcrypt = require('bcryptjs');
+const jwt  = require('jsonwebtoken')
+const Promise = require('bluebird')
+const crypto = Promise.promisifyAll(require('crypto'))
+const Helpers = require('../helpers')
+
+
 module.exports = {
    
 }
