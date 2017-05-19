47a22df6d76e31948e956e40bfab18d56b29d098
abe <abestbet1@gmail.com>
2017-05-19 12:35:52 -0700

    test function user controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,7 +5,10 @@ const Promise = require('bluebird')
 const crypto = Promise.promisifyAll(require('crypto'))
 const Helpers = require('../helpers')
 
+const test  = (req, res, next) => {
+	return res.status(200).json({ message: ' this is working' })	
+}
 
 module.exports = {
-   
+   test: test
 }
