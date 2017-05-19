d1009049966c481192a05b123905c9d14b4aa457
abe <abestbet1@gmail.com>
2017-05-19 12:42:34 -0700

    getAllUsers function

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,6 +9,25 @@ const test  = (req, res, next) => {
 	return res.status(200).json({ message: ' this is working' })	
 }
 
+const getAllUsers = (req, res, next)=> {
+   User.find({}, function( err, users) {
+     if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        res.status(200).json({
+            message: 'Successfully returned all user',
+            users: users            
+        });
+
+   })
+}
+
+
+
 module.exports = {
    routeTest: test
+   getAllUsers: getAllUsers
 }
