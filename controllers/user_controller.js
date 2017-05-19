9645ba37bad3aa3a8c81d482365c1082b4728c19
abe <abestbet1@gmail.com>
2017-05-19 12:45:45 -0700

    getOneUser controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -25,9 +25,25 @@ const getAllUsers = (req, res, next)=> {
    })
 }
 
+const getOneUser = ( req, res, next ) => {	
+  	User.findById( req.params.id , function(err, user){
+          if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        res.status(200).json({
+            message: 'Successfully returned all user',
+            user: user          
+        });
+      })
+}
+
 
 
 module.exports = {
    routeTest: test,
-   getAllUsers: getAllUsers
+   getAllUsers: getAllUsers,
+   getOneUser: getOneUser
 }
